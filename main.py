92beea9f6f98206c005571592617a2029c3b7c52
ivin jose <ivinnjose@gmail.com>
2024-12-18 12:37:10 +0530

    Model created and Model evaluation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
 
 
 ''' CREATEING DATASET '''
@@ -25,12 +29,10 @@ split_count = int(len(dataset) * 0.8)
 training_data = dataset[:split_count]
 testing_data = dataset[split_count:]
 
-# print(len(training_data), len(testing_data))
 
 ''' Divide the input and output from the '''
 
 
-# print(training_data)
 x_train = [data[0] for data in training_data]
 y_train = [data[1] for data in training_data]
 
@@ -48,4 +50,51 @@ y_train_scaled = scaler.fit_transform([[i] for i in y_train])
 
 x_test_scaled = scaler.fit_transform(x_test)
 y_test_scaled = scaler.fit_transform([[i] for i in y_test])
+
+
+''' Creating model for prediction '''
+
+x_train = np.array(x_train_scaled)
+y_train = np.array(y_train_scaled)
+
+x_test = np.array(x_test_scaled)
+y_test = np.array(y_test_scaled)
+
+''' Model '''
+
+model = Sequential([
+    layers.Dense(64, activation='relu', input_shape=(3,)),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(1)
+])
+
+# model.summary()
+
+''' Model Compiling '''
+
+model.compile(
+    optimizer = 'adam',
+    loss = 'mse',
+    metrics = ['mae']
+)
+
+''' Model Training '''
+
+trained = model.fit(
+    x_train, y_train,
+    validation_data = (x_test, y_test),
+    epochs = 100,
+    batch_size = 32,
+    verbose = 1
+)
+
+
+
+''' Model Evaluation '''
+
+loss,  mae = model.evaluate(x_test, y_test)
+print(f"Test Loss: {loss}, Test MAE: {mae}")
+
+
+
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(x_test[:5])
